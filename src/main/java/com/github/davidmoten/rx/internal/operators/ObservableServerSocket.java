bb7ca517f141889975efa8679c42497e0aad82b4
Dave Moten <davidmoten@gmail.com>
2016-09-06 08:54:49 +1000

    dev

--- src/main/java/com/github/davidmoten/rx/internal/operators/ObservableServerSocket.java
+++ src/main/java/com/github/davidmoten/rx/internal/operators/ObservableServerSocket.java
@@ -11,6 +11,7 @@ import java.nio.channels.CompletionHandler;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 import rx.AsyncEmitter;
@@ -25,207 +26,236 @@ import rx.functions.Func1;
 
 public final class ObservableServerSocket {
 
-	private ObservableServerSocket() {
-		// prevent instantiation
-	}
+    private ObservableServerSocket() {
+        // prevent instantiation
+    }
 
-	public static Observable<Observable<byte[]>> create(final int port, final long timeout, final TimeUnit unit,
-			final int bufferSize, BackpressureMode backpressureMode, Func0<AsynchronousChannelGroup> group) {
-		Func0<AsynchronousServerSocketChannel> serverSocketFactory = createServerSocketFactory(port, group);
-		Func1<AsynchronousServerSocketChannel, Observable<Observable<byte[]>>> serverSocketObservable = serverSocketChannel -> Observable
-				.create(new MyOnSubscribe(serverSocketChannel, unit.toMillis(timeout), bufferSize, backpressureMode));
-		// Observable.using handles closing of stuff on termination or
-		// unsubscription
-		return Observable.using(serverSocketFactory, serverSocketObservable, closer());
-	}
+    public static Observable<Observable<byte[]>> create(final int port, final long timeout,
+            final TimeUnit unit, final int bufferSize, BackpressureMode backpressureMode,
+            Func0<AsynchronousChannelGroup> group) {
+        Func0<AsynchronousServerSocketChannel> serverSocketFactory = createServerSocketFactory(port,
+                group);
+        Func1<AsynchronousServerSocketChannel, Observable<Observable<byte[]>>> serverSocketObservable = serverSocketChannel -> Observable
+                .create(new MyOnSubscribe(serverSocketChannel, unit.toMillis(timeout), bufferSize,
+                        backpressureMode));
+        // Observable.using handles closing of stuff on termination or
+        // unsubscription
+        return Observable.using(serverSocketFactory, serverSocketObservable, closer());
+    }
 
-	private static Func0<AsynchronousServerSocketChannel> createServerSocketFactory(final int port, Func0<AsynchronousChannelGroup> group) {
-		return () -> {
-			try {
-				AsynchronousChannelGroup g = group == null ? null : group.call();
-				return AsynchronousServerSocketChannel.open(g).bind(new InetSocketAddress(port));
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		};
-	}
+    private static Func0<AsynchronousServerSocketChannel> createServerSocketFactory(final int port,
+            Func0<AsynchronousChannelGroup> group) {
+        return () -> {
+            try {
+                AsynchronousChannelGroup g = group == null ? null : group.call();
+                return AsynchronousServerSocketChannel.open(g).bind(new InetSocketAddress(port));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 
-	private static final class MyOnSubscribe implements OnSubscribe<Observable<byte[]>> {
+    private static final class MyOnSubscribe implements OnSubscribe<Observable<byte[]>> {
 
-		private final AsynchronousServerSocketChannel serverSocketChannel;
-		private final long timeoutMs;
-		private final int bufferSize;
-		private final BackpressureMode backpressureMode;
+        private final AsynchronousServerSocketChannel serverSocketChannel;
+        private final long timeoutMs;
+        private final int bufferSize;
+        private final BackpressureMode backpressureMode;
 
-		MyOnSubscribe(AsynchronousServerSocketChannel serverSocketChannel, long timeoutMs, int bufferSize, BackpressureMode backpressureMode) {
-			this.serverSocketChannel = serverSocketChannel;
-			this.timeoutMs = timeoutMs;
-			this.bufferSize = bufferSize;
-			this.backpressureMode = backpressureMode;
-		}
+        MyOnSubscribe(AsynchronousServerSocketChannel serverSocketChannel, long timeoutMs,
+                int bufferSize, BackpressureMode backpressureMode) {
+            this.serverSocketChannel = serverSocketChannel;
+            this.timeoutMs = timeoutMs;
+            this.bufferSize = bufferSize;
+            this.backpressureMode = backpressureMode;
+        }
 
-		@Override
-		public void call(Subscriber<? super Observable<byte[]>> subscriber) {
-			subscriber.setProducer(new MyProducer(serverSocketChannel, timeoutMs, bufferSize, subscriber, backpressureMode));
-		}
+        @Override
+        public void call(Subscriber<? super Observable<byte[]>> subscriber) {
+            subscriber.setProducer(new MyProducer(serverSocketChannel, timeoutMs, bufferSize,
+                    subscriber, backpressureMode));
+        }
 
-	}
+    }
 
-	private static final class MyProducer implements CompletionHandler<AsynchronousSocketChannel, Void>, Producer {
+    private static final class MyProducer
+            implements CompletionHandler<AsynchronousSocketChannel, Void>, Producer {
 
-		private final AsynchronousServerSocketChannel serverSocketChannel;
-		private final long timeoutMs;
-		private final int bufferSize;
-		private final Subscriber<? super Observable<byte[]>> subscriber;
-		private final BackpressureMode backpressureMode;
+        private final AsynchronousServerSocketChannel serverSocketChannel;
+        private final long timeoutMs;
+        private final int bufferSize;
+        private final Subscriber<? super Observable<byte[]>> subscriber;
+        private final BackpressureMode backpressureMode;
+        private final AtomicLong requested = new AtomicLong();
 
-		public MyProducer(AsynchronousServerSocketChannel serverSocketChannel, long timeoutMs, int bufferSize,
-				Subscriber<? super Observable<byte[]>> subscriber, BackpressureMode backpressureMode) {
-			this.serverSocketChannel = serverSocketChannel;
-			this.timeoutMs = timeoutMs;
-			this.bufferSize = bufferSize;
-			this.subscriber = subscriber;
-			this.backpressureMode = backpressureMode;
-		}
+        public MyProducer(AsynchronousServerSocketChannel serverSocketChannel, long timeoutMs,
+                int bufferSize, Subscriber<? super Observable<byte[]>> subscriber,
+                BackpressureMode backpressureMode) {
+            this.serverSocketChannel = serverSocketChannel;
+            this.timeoutMs = timeoutMs;
+            this.bufferSize = bufferSize;
+            this.subscriber = subscriber;
+            this.backpressureMode = backpressureMode;
+        }
 
-		private static final class State {
-			final boolean accepting;
-			final long requested;
+        private static final class State {
+            final long accepting;
+            final long requested;
 
-			State(boolean accepting, long requested) {
-				this.accepting = accepting;
-				this.requested = requested;
-			}
+            State(long accepting, long requested) {
+                this.accepting = accepting;
+                this.requested = requested;
+            }
 
-			static State create(boolean accepting, long requested) {
-				return new State(accepting, requested);
-			}
-		}
+            static State create(long accepting, long requested) {
+                return new State(accepting, requested);
+            }
 
-		private final AtomicReference<State> state = new AtomicReference<State>(new State(false, 0));
+            @Override
+            public String toString() {
+                return "State [accepting=" + accepting + ", requested=" + requested + "]";
+            }
 
-		@Override
-		public void request(long n) {
-			if (n <= 0)
-				return;
+        }
 
-			while (true) {
-				State s = state.get();
-				long r = s.requested + n;
-				if (r < 0) {
-					r = Long.MAX_VALUE;
-				}
-				final State s2;
-				boolean accept = !s.accepting && r > 0;
-				if (accept) {
-					s2 = State.create(true, r);
-				} else {
-					s2 = State.create(s.accepting, r);
-				}
-				if (state.compareAndSet(s, s2)) {
-					if (accept) {
-						serverSocketChannel.accept(null, this);
-					}
-					break;
-				}
-			}
-		}
+        private final AtomicReference<State> state = new AtomicReference<State>(new State(0, 0));
 
-		private void checkRequests() {
-			while (true) {
-				State s = state.get();
-				long r = s.requested;
-				final State s2;
-				boolean accept = !s.accepting && r > 0;
-				if (accept) {
-					s2 = new State(true, r);
-				} else {
-					s2 = new State(s.accepting, r);
-				}
-				if (state.compareAndSet(s, s2)) {
-					if (accept) {
-						serverSocketChannel.accept(null, this);
-					}
-					break;
-				}
-			}
-		}
+        @Override
+        public void request(long n) {
+            if (n <= 0)
+                return;
 
-		@Override
-		public void completed(AsynchronousSocketChannel socketChannel, Void attachment) {
+            // use CAS loop to safely update state
+            while (true) {
+                State s = state.get();
+                long r = s.requested + n;
+                if (r < 0) {
+                    r = Long.MAX_VALUE;
+                }
+                boolean accept = s.accepting == 0 && r > 0;
+                State s2 = State.create(s.accepting + 1, r);
+                if (state.compareAndSet(s, s2)) {
+                    if (accept) {
+                        System.out.println("accepting via request for " + n);
+                        serverSocketChannel.accept(null, this);
+                    } else {
+                        System.out.println("not ready to accept via request for " + n);
+                    }
+                    System.out.println("state=" + state);
+                    break;
+                }
+            }
+        }
 
-			checkRequests();
+        private void checkRequests() {
+            // use CAS loop to safely update state
+            while (true) {
+                State s = state.get();
+                long r = s.requested;
+                boolean accept = r > 0;
+                final State s2;
+                if (accept)
+                    s2 = new State(s.accepting - 1, decrement(r));
+                else
+                    s2 = new State(s.accepting, r);
+                if (state.compareAndSet(s, s2)) {
+                    if (accept) {
+                        System.out.println("accepting via check ");
+                        serverSocketChannel.accept(null, this);
+                    } else {
+                        System.out.println("not ready to accept via check");
+                    }
+                    System.out.println("state=" + state);
+                    break;
+                }
+            }
+        }
 
-			Action1<AsyncEmitter<byte[]>> emitterAction = new Action1<AsyncEmitter<byte[]>>() {
+        private static long decrement(long r) {
+            if (r == Long.MAX_VALUE) {
+                return r;
+            } else {
+                return r - 1;
+            }
+        }
 
-				volatile boolean done;
+        @Override
+        public void completed(AsynchronousSocketChannel socketChannel, Void attachment) {
+            System.out.println("socket connected");
 
-				@Override
-				public void call(AsyncEmitter<byte[]> emitter) {
-					emitter.setCancellation(() -> {
-						done = true;
-						socketChannel.close();
-					});
+            checkRequests();
 
-					// Allocate a byte buffer to read from the client
-					ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-					try {
-						int bytesRead;
-						while (!done && (bytesRead = socketChannel.read(buffer).get(timeoutMs,
-								TimeUnit.MILLISECONDS)) != -1) {
-							// check the value of done again because the read
-							// may have taken some time (close to the timeout)
-							if (done) {
-								return;
-							}
+            Action1<AsyncEmitter<byte[]>> emitterAction = new Action1<AsyncEmitter<byte[]>>() {
 
-							// Make the buffer ready to read
-							buffer.flip();
+                volatile boolean done;
 
-							// copy the current buffer to a byte array
-							byte[] chunk = new byte[bytesRead];
-							buffer.get(chunk, 0, bytesRead);
+                @Override
+                public void call(AsyncEmitter<byte[]> emitter) {
+                    emitter.setCancellation(() -> {
+                        done = true;
+                        // pull the plug on a blocking read
+                        socketChannel.close();
+                    });
 
-							// emit the chunk
-							emitter.onNext(chunk);
+                    // Allocate a byte buffer to read from the client
+                    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+                    try {
+                        int bytesRead;
+                        while (!done && (bytesRead = socketChannel.read(buffer).get(timeoutMs,
+                                TimeUnit.MILLISECONDS)) != -1) {
+                            // check the value of done again because the read
+                            // may have taken some time (waiting for timeout)
+                            if (done) {
+                                return;
+                            }
 
-							// Make the buffer ready to write
-							buffer.clear();
-						}
-						if (!done) {
-							emitter.onCompleted();
-						}
-					} catch (InterruptedException | ExecutionException | TimeoutException e) {
-						emitter.onError(e);
-					}
+                            // Make the buffer ready to read
+                            buffer.flip();
 
-				}
-			};
+                            // copy the current buffer to a byte array
+                            byte[] chunk = new byte[bytesRead];
+                            buffer.get(chunk, 0, bytesRead);
 
-			Observable<byte[]> obs = Observable.fromAsync(emitterAction, backpressureMode);
-			if (!subscriber.isUnsubscribed()) {
-				subscriber.onNext(obs);
-			}
-		}
+                            // emit the chunk
+                            emitter.onNext(chunk);
 
-		@Override
-		public void failed(Throwable e, Void attachment) {
-			if (!subscriber.isUnsubscribed()) {
-				subscriber.onNext(Observable.error(e));
-			}
-		}
+                            // Make the buffer ready to write
+                            buffer.clear();
+                        }
+                        if (!done) {
+                            emitter.onCompleted();
+                        }
+                    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                        emitter.onError(e);
+                    }
 
-	}
+                }
+            };
 
-	// Visible for testing
-	static Action1<Closeable> closer() {
-		return c -> {
-			try {
-				c.close();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		};
-	}
+            Observable<byte[]> obs = Observable.fromEmitter(emitterAction, backpressureMode);
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onNext(obs);
+            }
+        }
+
+        @Override
+        public void failed(Throwable e, Void attachment) {
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onNext(Observable.error(e));
+            }
+        }
+
+    }
+
+    // Visible for testing
+    static Action1<Closeable> closer() {
+        return c -> {
+            try {
+                c.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
 
 }
--- src/test/java/com/github/davidmoten/rx/internal/operators/ObservableServerSocketTest.java
+++ src/test/java/com/github/davidmoten/rx/internal/operators/ObservableServerSocketTest.java
@@ -11,6 +11,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
@@ -23,124 +27,175 @@ import com.github.davidmoten.rx.Actions;
 import com.github.davidmoten.rx.Bytes;
 import com.github.davidmoten.rx.IO;
 
-import rx.Observable;
 import rx.AsyncEmitter.BackpressureMode;
+import rx.Observable;
 import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
 
 public final class ObservableServerSocketTest {
 
-	private static final int PORT = 12345;
-	private static final String TEXT = "hello there";
+    private static final int PORT = 12345;
+    private static final String TEXT = "hello there";
 
-	@Test
-	public void serverSocketReadsTcpPushWhenBufferIsSmallerThanInput()
-			throws UnknownHostException, IOException, InterruptedException {
-		checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, 4);
-	}
+    @Test
+    public void serverSocketReadsTcpPushWhenBufferIsSmallerThanInput()
+            throws UnknownHostException, IOException, InterruptedException {
+        checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, 4);
+    }
 
-	@Test
-	public void serverSocketReadsTcpPushWhenBufferIsBiggerThanInput()
-			throws UnknownHostException, IOException, InterruptedException {
-		checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, 8192);
-	}
+    @Test
+    public void serverSocketReadsTcpPushWhenBufferIsBiggerThanInput()
+            throws UnknownHostException, IOException, InterruptedException {
+        checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, 8192);
+    }
 
-	@Test
-	public void serverSocketReadsTcpPushWhenBufferIsSameSizeAsInput()
-			throws UnknownHostException, IOException, InterruptedException {
-		checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, TEXT.length());
-	}
+    @Test
+    public void serverSocketReadsTcpPushWhenBufferIsSameSizeAsInput()
+            throws UnknownHostException, IOException, InterruptedException {
+        checkServerSocketReadsTcpPushWhenBufferSizeIs(TEXT, TEXT.length());
+    }
 
-	@Test
-	public void serverSocketReadsTcpPushWhenInputIsEmpty()
-			throws UnknownHostException, IOException, InterruptedException {
-		checkServerSocketReadsTcpPushWhenBufferSizeIs("", 4);
-	}
+    @Test
+    public void serverSocketReadsTcpPushWhenInputIsEmpty()
+            throws UnknownHostException, IOException, InterruptedException {
+        checkServerSocketReadsTcpPushWhenBufferSizeIs("", 4);
+    }
 
-	@Test
-	public void serverSocketReadsTcpPushWhenInputIsOneCharacter()
-			throws UnknownHostException, IOException, InterruptedException {
-		checkServerSocketReadsTcpPushWhenBufferSizeIs("a", 4);
-	}
+    @Test
+    public void serverSocketReadsTcpPushWhenInputIsOneCharacter()
+            throws UnknownHostException, IOException, InterruptedException {
+        checkServerSocketReadsTcpPushWhenBufferSizeIs("a", 4);
+    }
 
-	@Test
-	public void errorEmittedIfServerSocketBusy() throws IOException {
+    @Test
+    public void errorEmittedIfServerSocketBusy() throws IOException {
 
-		TestSubscriber<Object> ts = TestSubscriber.create();
-		try (ServerSocket socket = new ServerSocket(PORT)) {
-			IO.serverSocket(PORT, 10, TimeUnit.SECONDS, 5, BackpressureMode.BUFFER).subscribe(ts);
-			ts.assertNoValues();
-			ts.assertNotCompleted();
-			ts.assertTerminalEvent();
-			assertTrue(ts.getOnErrorEvents().get(0).getCause() instanceof BindException);
-		}
-	}
+        TestSubscriber<Object> ts = TestSubscriber.create();
+        try (ServerSocket socket = new ServerSocket(PORT)) {
+            IO.serverSocket(PORT, 10, TimeUnit.SECONDS, 5, BackpressureMode.BUFFER).subscribe(ts);
+            ts.assertNoValues();
+            ts.assertNotCompleted();
+            ts.assertTerminalEvent();
+            assertTrue(ts.getOnErrorEvents().get(0).getCause() instanceof BindException);
+        }
+    }
 
-	@Test
-	public void isUtilityClass() {
-		Asserts.assertIsUtilityClass(ObservableServerSocket.class);
-	}
+    @Test
+    public void isUtilityClass() {
+        Asserts.assertIsUtilityClass(ObservableServerSocket.class);
+    }
 
-	@Test
-	public void isUtilityClassIO() {
-		Asserts.assertIsUtilityClass(IO.class);
-	}
+    @Test
+    public void isUtilityClassIO() {
+        Asserts.assertIsUtilityClass(IO.class);
+    }
 
-	@Test
-	public void testCloserWhenDoesNotThrow() {
-		AtomicBoolean called = new AtomicBoolean();
-		Closeable c = new Closeable() {
+    @Test
+    public void testCloserWhenDoesNotThrow() {
+        AtomicBoolean called = new AtomicBoolean();
+        Closeable c = new Closeable() {
 
-			@Override
-			public void close() throws IOException {
-				called.set(true);
-			}
-		};
-		ObservableServerSocket.closer().call(c);
-		assertTrue(called.get());
-	}
+            @Override
+            public void close() throws IOException {
+                called.set(true);
+            }
+        };
+        ObservableServerSocket.closer().call(c);
+        assertTrue(called.get());
+    }
 
-	@Test
-	public void testCloserWhenThrows() {
-		IOException ex = new IOException();
-		Closeable c = new Closeable() {
+    @Test
+    public void testCloserWhenThrows() {
+        IOException ex = new IOException();
+        Closeable c = new Closeable() {
 
-			@Override
-			public void close() throws IOException {
-				throw ex;
-			}
-		};
-		try {
-			ObservableServerSocket.closer().call(c);
-			Assert.fail();
-		} catch (RuntimeException e) {
-			assertTrue(ex == e.getCause());
-		}
-	}
+            @Override
+            public void close() throws IOException {
+                throw ex;
+            }
+        };
+        try {
+            ObservableServerSocket.closer().call(c);
+            Assert.fail();
+        } catch (RuntimeException e) {
+            assertTrue(ex == e.getCause());
+        }
+    }
 
-	private void checkServerSocketReadsTcpPushWhenBufferSizeIs(String text, int bufferSize)
-			throws UnknownHostException, IOException, InterruptedException {
-		TestSubscriber<Object> ts = TestSubscriber.create();
-		AtomicReference<byte[]> result = new AtomicReference<byte[]>();
-		try {
-			IO.serverSocket(PORT, 10, TimeUnit.SECONDS, bufferSize, BackpressureMode.BUFFER) //
-					.flatMap(g -> g //
-							.compose(Bytes.collect()) //
-							.doOnNext(Actions.setAtomic(result)) //
-							.doOnNext(bytes -> System.out
-									.println(Thread.currentThread().getName() + ": " + new String(bytes))) //
-							.onErrorResumeNext(Observable.empty()))
-					.subscribe(ts);
-			Socket socket = new Socket("localhost", 12345);
-			OutputStream out = socket.getOutputStream();
-			out.write(text.getBytes());
-			out.close();
-			socket.close();
-			Thread.sleep(1000);
-			assertEquals(text, new String(result.get(), StandardCharsets.UTF_8));
-		} finally {
-			// will close server socket
-			ts.unsubscribe();
-		}
-	}
+    @Test
+    public void testAsynchronousDelivery() throws InterruptedException {
+        TestSubscriber<String> ts = TestSubscriber.create();
+        try {
+            int bufferSize = 4;
+            IO.serverSocket(PORT, 10, TimeUnit.SECONDS, bufferSize, BackpressureMode.BUFFER) //
+                    .flatMap(g -> g //
+                            .compose(Bytes.collect()) //
+                            .onErrorResumeNext(Observable.empty())) //
+                    .map(bytes -> new String(bytes, StandardCharsets.UTF_8)) //
+                    .doOnNext(System.out::println) //
+                    .doOnError(e -> e.printStackTrace()) //
+                    .subscribe(ts);
+            TestSubscriber<Object> ts2 = TestSubscriber.create();
+            Set<String> messages = new ConcurrentSkipListSet<>();
+            int messageBlocks = 1;
+            int numMessages = 20;
+            Observable.range(1, numMessages).flatMap(n -> {
+                return Observable.defer(() -> {
+                    System.out.println("writing message");
+                    String id = UUID.randomUUID().toString();
+                    StringBuilder s = new StringBuilder();
+                    for (int i = 0; i < messageBlocks; i++) {
+                        s.append(id);
+                    }
+                    messages.add(s.toString());
+                    try {
+                        Socket socket = new Socket("localhost", PORT);
+                        OutputStream out = socket.getOutputStream();
+                        for (int i = 0; i < messageBlocks; i++) {
+                            out.write(id.getBytes(StandardCharsets.UTF_8));
+                        }
+                        out.close();
+                        socket.close();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return Observable.just(1);
+                }).subscribeOn(Schedulers.computation());
+            } , 1).subscribe(ts2);
+            ts2.awaitTerminalEvent();
+            ts2.assertCompleted();
+            // allow server to complete processing
+            Thread.sleep(1000);
+            assertEquals(messages, new HashSet<String>(ts.getOnNextEvents()));
+        } finally {
+            ts.unsubscribe();
+        }
+    }
+
+    private void checkServerSocketReadsTcpPushWhenBufferSizeIs(String text, int bufferSize)
+            throws UnknownHostException, IOException, InterruptedException {
+        TestSubscriber<Object> ts = TestSubscriber.create();
+        AtomicReference<byte[]> result = new AtomicReference<byte[]>();
+        try {
+            IO.serverSocket(PORT, 10, TimeUnit.SECONDS, bufferSize, BackpressureMode.BUFFER) //
+                    .flatMap(g -> g //
+                            .compose(Bytes.collect()) //
+                            .doOnNext(Actions.setAtomic(result)) //
+                            .doOnNext(bytes -> System.out.println(
+                                    Thread.currentThread().getName() + ": " + new String(bytes))) //
+                            .onErrorResumeNext(Observable.empty()))
+                    .subscribe(ts);
+            Socket socket = new Socket("localhost", PORT);
+            OutputStream out = socket.getOutputStream();
+            out.write(text.getBytes());
+            out.close();
+            socket.close();
+            Thread.sleep(1000);
+            assertEquals(text, new String(result.get(), StandardCharsets.UTF_8));
+        } finally {
+            // will close server socket
+            ts.unsubscribe();
+        }
+    }
 
 }
